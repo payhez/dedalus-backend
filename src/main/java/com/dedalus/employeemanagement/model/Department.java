a0bb191278163306dfb6957445ff0d858fbc6bd9
payhez <payhez@gmail.com>
2025-05-27 09:19:18 +0200

    add entities

---
+++ src/main/java/com/dedalus/employeemanagement/model/Department.java
@@ -0,0 +1,23 @@
+package com.dedalus.employeemanagement.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Department {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    @NotBlank(message = "Department name is required")
+    private String name;
+}
---
+++ src/main/java/com/dedalus/employeemanagement/model/Employee.java
@@ -0,0 +1,39 @@
+package com.dedalus.employeemanagement.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Pattern;
+import org.springframework.data.annotation.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Builder
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Full name is required")
+    private String fullName;
+
+    private String address;
+
+    @Pattern(regexp = "^\\+?[0-9\\s-]+$", message = "Invalid phone number format")
+    private String phoneNumber;
+
+    @Email(message = "Invalid email format")
+    private String email;
+
+    @ManyToOne
+    @JoinColumn(name = "department_id")
+    private Department department;
+}
